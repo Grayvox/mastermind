eb340f89c40e8b7ff5cc277ea54a35079d112383
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-11 19:25:41 -0400

    Add guess comparison method

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,5 +2,15 @@
 
 # Houses the general game logic and methods
 module GameLogic
-  
+  def compare_guess(code, guess)
+    results = []
+    guess.each_with_index do |guess_el, guess_i|
+      results.push('☑') if code[guess_i] == guess[guess_i]
+      results.push('☒') unless code.include?(guess_el)
+      unless code.count(guess_el) < guess.count(guess_el)
+        results.push('☐') if code[guess_i] != guess[guess_i]
+      end
+    end
+    results.join(' ')
+  end
 end
