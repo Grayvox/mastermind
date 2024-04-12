bcd4782dbc999a4f6119858579a0240b3e78a4da
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-11 21:03:14 -0400

    Add computer code generator

--- lib/human_solver.rb
+++ lib/human_solver.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
-require_relative 'game'
+require_relative './game'
 
 # Defines a human trying to crack the code
 class HumanSolver < Game
+  attr_accessor :computer_code
+
   def initialize
     super
+    @computer_code = generate_code(@colors)
+  end
+
+  def generate_code(options = {})
+    options.values.to_a.sample(4)
   end
 end
