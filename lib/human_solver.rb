9b7915af2bd895f73fbe7a013d3c6a1c67790eec
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-13 14:54:00 -0400

    Add guess functionality and color checker
    * Adds the human_guess method to allow a human
    player to make a guess
    
    * Adds the color_check method to check if the
    human's input is a valid color

--- lib/human_solver.rb
+++ lib/human_solver.rb
@@ -14,4 +14,41 @@ class HumanSolver < Game
   def generate_code(options = {})
     options.values.to_a.sample(4)
   end
+
+  def human_guess
+    total_guesses = 0
+    guess = []
+    until total_guesses == 4
+      puts "Input the next color in your guess. (#{total_guesses + 1}/4)"
+      current_color = gets.chomp.downcase
+      check_color = color_check(current_color)
+      next guess.push(check_color) && total_guesses += 1 unless check_color == 'Invalid color'
+
+      puts 'Invalid color name. Please try again.'
+    end
+    guess
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def color_check(input)
+    case input
+    when 'r', 'red'
+      @colors[:red]
+    when 'g', 'green'
+      @colors[:green]
+    when 'y', 'yellow'
+      @colors[:yellow]
+    when 'b', 'blue'
+      @colors[:blue]
+    when 'p', 'purple'
+      @colors[:purple]
+    when 'c', 'cyan'
+      @colors[:cyan]
+    else
+      'Invalid color'
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
 end
+
+puts HumanSolver.new.human_guess
